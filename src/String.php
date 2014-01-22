0e1c6dbbe16ecaa5efcbf67e1a9491a1aa1f4075
Ross Riley <riley.ross@gmail.com>
2014-01-22 10:45:07 +0000

    work on implementing PSR-2 standards

--- src/String.php
+++ src/String.php
@@ -1,8 +1,11 @@
 <?php
 namespace Spl\Scalars;
 
-class String {
-    public function length() {
+class String
+{
+
+    public function length()
+    {
         return strlen($this);
     }
 
@@ -10,7 +13,8 @@ class String {
      * Slicing methods
      */
 
-    public function slice($offset, $length = null) {
+    public function slice($offset, $length = null)
+    {
         $offset = $this->prepareOffset($offset);
         $length = $this->prepareLength($offset, $length);
 
@@ -21,7 +25,8 @@ class String {
         return substr($this, $offset, $length);
     }
 
-    public function replaceSlice($replacement, $offset, $length = null) {
+    public function replaceSlice($replacement, $offset, $length = null)
+    {
         $offset = $this->prepareOffset($offset);
         $length = $this->prepareLength($offset, $length);
 
@@ -32,7 +37,8 @@ class String {
      * Search methods
      */
 
-    public function indexOf($string, $offset = 0) {
+    public function indexOf($string, $offset = 0)
+    {
         $offset = $this->prepareOffset($offset);
 
         if ('' === $string) {
@@ -42,7 +48,8 @@ class String {
         return strpos($this, $string, $offset);
     }
 
-    public function lastIndexOf($string, $offset = null) {
+    public function lastIndexOf($string, $offset = null)
+    {
         if (null === $offset) {
             $offset = $this->length();
         } else {
@@ -58,19 +65,23 @@ class String {
         return strrpos($this, $string, $offset - $this->length());
     }
 
-    public function contains($string) {
+    public function contains($string)
+    {
         return false !== $this->indexOf($string);
     }
 
-    public function startsWith($string) {
+    public function startsWith($string)
+    {
         return 0 === $this->indexOf($string);
     }
 
-    public function endsWith($string) {
+    public function endsWith($string)
+    {
         return $this->lastIndexOf($string) === $this->length() - $string->length();
     }
 
-    public function count($string, $offset = 0, $length = null) {
+    public function count($string, $offset = 0, $length = null)
+    {
         $offset = $this->prepareOffset($offset);
         $length = $this->prepareLength($offset, $length);
 
@@ -86,7 +97,8 @@ class String {
      * replace(array(string $from => string $to) $replacements, int $limit = PHP_MAX_INT)
      * replace(string $from, string $to, int $limit = PHP_MAX_INT)
      */
-    public function replace($from, $to = null, $limit = null) {
+    public function replace($from, $to = null, $limit = null)
+    {
         if (is_array($from)) {
             return $this->replacePairs($from, $to);
         }
@@ -99,78 +111,95 @@ class String {
         return $this->replaceWithLimit($this, $from, $to, $limit);
     }
 
-    public function split($separator, $limit = PHP_INT_MAX) {
+    public function split($separator, $limit = PHP_INT_MAX)
+    {
         return explode($separator, $this, $limit);
     }
 
-    public function chunk($chunkLength = 1) {
+    public function chunk($chunkLength = 1)
+    {
         $this->verifyPositive($chunkLength, 'Chunk length');
         return str_split($this, $chunkLength);
     }
 
-    public function repeat($times) {
+    public function repeat($times)
+    {
         $this->verifyNotNegative($times, 'Number of repetitions');
         return str_repeat($this, $times);
     }
 
-    public function reverse() {
+    public function reverse()
+    {
         return strrev($this);
     }
 
 
-    public function trim($characters = " \t\n\r\v\0") {
+    public function trim($characters = " \t\n\r\v\0")
+    {
         return trim($this, $characters);
     }
 
-    public function trimLeft($characters = " \t\n\r\v\0") {
+    public function trimLeft($characters = " \t\n\r\v\0")
+    {
         return ltrim($this, $characters);
     }
 
-    public function trimRight($characters = " \t\n\r\v\0") {
+    public function trimRight($characters = " \t\n\r\v\0")
+    {
         return rtrim($this, $characters);
     }
 
-    public function padLeft($length, $padString = " ") {
+    public function padLeft($length, $padString = " ")
+    {
         return str_pad($this, $length, $padString, STR_PAD_LEFT);
     }
 
-    public function padRight($length, $padString = " ") {
+    public function padRight($length, $padString = " ")
+    {
         return str_pad($this, $length, $padString, STR_PAD_RIGHT);
     }
 
 
-    public function toArray() {
+    public function toArray()
+    {
       return [$this];
     }
 
-    public function toFloat() {
+    public function toFloat()
+    {
       return float($this);
     }
 
-    public function toInt() {
+    public function toInt()
+    {
       return int($this);
     }
 
-    public function toJSON() {
+    public function toJSON()
+    {
       return json_encode($this);
     }
 
-    public function lower() {
+    public function lower()
+    {
         return strtolower($this);
     }
 
-    public function toString() {
-      return $this;
+    public function toString()
+    {
+        return $this;
     }
 
-    public function upper() {
+    public function upper()
+    {
         return strtoupper($this);
     }
 
 
     // Internal methods, not part of public API
 
-    protected function prepareOffset($offset) {
+    protected function prepareOffset($offset)
+    {
         $len = $this->length();
         if ($offset < -$len || $offset > $len) {
             throw new \InvalidArgumentException('Offset must be in range [-len, len]');
@@ -183,7 +212,8 @@ class String {
         return $offset;
     }
 
-    protected function prepareLength($offset, $length) {
+    protected function prepareLength($offset, $length)
+    {
         if (null === $length) {
             return $this->length() - $offset;
         }
@@ -203,19 +233,22 @@ class String {
         return $length;
     }
 
-    protected function verifyPositive($value, $name) {
+    protected function verifyPositive($value, $name)
+    {
         if ($value <= 0) {
             throw new \InvalidArgumentException("$name has to be positive");
         }
     }
 
-    protected function verifyNotNegative($value, $name) {
+    protected function verifyNotNegative($value, $name)
+    {
         if ($value < 0) {
             throw new \InvalidArgumentException("$name can not be negative");
         }
     }
 
-    protected function replacePairs($replacements, $limit) {
+    protected function replacePairs($replacements, $limit)
+    {
         if (null === $limit) {
             return strtr($this, $replacements);
         }
@@ -231,7 +264,8 @@ class String {
         return $str;
     }
 
-    protected function replaceWithLimit($str, $from, $to, &$limit) {
+    protected function replaceWithLimit($str, $from, $to, &$limit)
+    {
         $lenDiff = $to->length() - $from->length();
         $index = 0;
 
