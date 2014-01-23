a5c5de0b6c8595b510b7b307e200754c06885610
Ross Riley <riley.ross@gmail.com>
2014-01-23 10:07:12 +0000

    hack not needed since phpunit autoloads

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,13 +1,6 @@
 <?php
 require __DIR__."/../vendor/autoload.php";
 
-// Hack to autoload the classes
-$init = new Spl\Scalars\StringHandler;
-$init = new Spl\Scalars\IntegerHandler;
-$init = new Spl\Scalars\FloatHandler;
-$init = new Spl\Scalars\ArrayHandler;
-$init = new Spl\Scalars\NullHandler;
-
 register_primitive_type_handler('string', 'Spl\\Scalars\\StringHandler');
 register_primitive_type_handler('int',    'Spl\\Scalars\\IntegerHandler');
 register_primitive_type_handler('float',  'Spl\\Scalars\\FloatHandler');
