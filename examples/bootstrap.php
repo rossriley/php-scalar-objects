a1feec95d2173ec09055833c171da6c59c47df00
Chris <c.kruining@fyn.nl>
2017-11-07 17:05:30 +0100

    the non-hack method

--- examples/bootstrap.php
+++ examples/bootstrap.php
@@ -1,19 +1,19 @@
 <?php
+
+use Spl\Scalars\StringHandler;
+use Spl\Scalars\IntegerHandler;
+use Spl\Scalars\FloatHandler;
+use Spl\Scalars\ArrayHandler;
+use Spl\Scalars\NullHandler;
+
 include __DIR__."/../vendor/autoload.php";
-// Hack to autoload the classes
-$init = new Spl\Scalars\StringHandler;
-$init = new Spl\Scalars\IntegerHandler;
-$init = new Spl\Scalars\FloatHandler;
-$init = new Spl\Scalars\ArrayHandler;
-$init = new Spl\Scalars\NullHandler;
 
-register_primitive_type_handler('string', 'Spl\\Scalars\\StringHandler');
-register_primitive_type_handler('int',    'Spl\\Scalars\\IntegerHandler');
-register_primitive_type_handler('float',  'Spl\\Scalars\\FloatHandler');
-register_primitive_type_handler('array',  'Spl\\Scalars\\ArrayHandler');
-register_primitive_type_handler('null',   'Spl\\Scalars\\NullHandler');
-register_primitive_type_handler('bool',   'Spl\\Scalars\\BooleanHandler');
-
+register_primitive_type_handler('string', Spl\Scalars\StringHandler::class);
+register_primitive_type_handler('int',    Spl\Scalars\IntegerHandler::class);
+register_primitive_type_handler('float',  Spl\Scalars\FloatHandler::class);
+register_primitive_type_handler('array',  Spl\Scalars\ArrayHandler::class);
+register_primitive_type_handler('null',   Spl\Scalars\NullHandler::class);
+register_primitive_type_handler('bool',   Spl\Scalars\BooleanHandler::class);
 
 function showCommand($title, $input, $command, $output) {
   echo PHP_EOL;
